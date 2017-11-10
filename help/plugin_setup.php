f4989dc32ed3b69f6726b965b31102ffa4b4e78f
Jared B <jaredb7@internode.on.net>
2017-11-10 22:37:12 +1000

    quick test of plugin update function

--- help/plugin_setup.php
+++ help/plugin_setup.php
@@ -5,12 +5,7 @@
 $gitURL = "https://github.com/jaredb7/FPP-VotingAPI-Integration.git";
 
 ?>
-
-<p>To report a bug, please file it against the <a
-        href="<? echo $gitURL ?>">FPP-VotingAPI-Integration</a> plugin
-    project on
-    Git: <? echo $gitURL ?> <br>
-    or Add to my <a href="https://trello.com/b/G5VbDFuQ/displays-near-me-public">Trello Board</a>
+<p>To report a bug, please file it against the <a href="<? echo $gitURL ?>">FPP-VotingAPI-Integration</a> plugin project
+    on Git: <? echo $gitURL ?>
 </p>
-
-
+<p>Add to my <a href="https://trello.com/b/G5VbDFuQ/displays-near-me-public">Trello Board</a></p>
