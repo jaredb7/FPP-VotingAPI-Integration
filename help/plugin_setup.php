8c2750e46d50ae53657c8ae3ea6f0430cad840d7
Jared B <jaredb7@internode.on.net>
2017-11-10 22:19:35 +1000

    Help file that no one ever uses, I put stuff in it this time
    Help file that no one ever uses, I put stuff in it this time

---
+++ help/plugin_setup.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Plugin Github URL
+ */
+$gitURL = "https://github.com/jaredb7/FPP-VotingAPI-Integration.git";
+
+?>
+
+<p>To report a bug, please file it against the <a
+        href="<? echo $gitURL ?>">FPP-VotingAPI-Integration</a> plugin
+    project on
+    Git: <? echo $gitURL ?> <br>
+    or Add to my <a href="https://trello.com/b/G5VbDFuQ/displays-near-me-public">Trello Board</a>
+</p>
+
+
